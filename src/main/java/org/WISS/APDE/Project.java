752ce5f31e19d270d3a706a8514239f3f9a76b5f
Benjamin <benjain.timon.chambre@gmai.com>
2025-09-04 20:17:10 +0200

    init

---
+++ src/main/java/org/WISS/APDE/Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.WISS.APDE;
+
+/**
+ *
+ * @author bcham
+ */
+public class Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
